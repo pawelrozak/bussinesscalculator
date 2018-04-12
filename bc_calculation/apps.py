3d26e5c0a206934ac8269bea7b24402406cc3fde
rozacek <rozak.pawel@gmail.com>
2018-04-12 16:13:42 +0200

    Many renames and adding my first app.

---
+++ bc_calculation/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BussinessCalculatorConfig(AppConfig):
+    name = 'bc_calculation'
--- bc_entry/urls.py
+++ bc_entry/urls.py
@@ -5,13 +5,11 @@ from django.contrib import admin
 admin.autodiscover()
 
 import hello.views
-
-# Examples:
-# url(r'^$', 'gettingstarted.views.home', name='home'),
-# url(r'^blog/', include('blog.urls')),
+from bc_calculation import urls as bc_calculation_urls
 
 urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
+    path('calculation/', include('bc_calculation.urls')),
     path('admin/', admin.site.urls),
 ]
